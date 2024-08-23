8d0444baa8f75342048e0bcdee42366e89547a3a
mazimi surface <mohammad.azimi.1996@gmail.com>
2024-08-23 21:15:17 +0330

    initial commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace HelloDotNet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController(
+        IMemoryCache memoryCache,
+        IDistributedCache redisCache,
+        IHttpClientFactory httpClientFactory) : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache = memoryCache;
+        private readonly IDistributedCache _redisCache = redisCache;
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBookById(string id)
+        {
+            var cacheKey = $"book_{id}";
+            var dataOrigin = "none";
+
+            if (_memoryCache.TryGetValue(cacheKey, out string bookContent))
+            {
+                dataOrigin = "memory";
+            }
+
+            if (bookContent == null)
+            {
+                var redisContent = await _redisCache.GetStringAsync(cacheKey);
+                if (redisContent != null)
+                {
+                    _memoryCache.Set(cacheKey, redisContent, TimeSpan.FromMinutes(5));
+                    bookContent = redisContent;
+                    dataOrigin = "redis_cache";
+                }
+            }
+
+            if (bookContent == null)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var apiUrl = $"https://get.taaghche.com/v2/book/{id}";
+                var response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    bookContent = await response.Content.ReadAsStringAsync();
+                    var cacheEntryOptions = new MemoryCacheEntryOptions()
+                        .SetAbsoluteExpiration(TimeSpan.FromSeconds(300));
+
+                    await _redisCache.SetStringAsync(cacheKey, bookContent);
+
+                    _memoryCache.Set(cacheKey, bookContent, cacheEntryOptions);
+
+                    dataOrigin = "upstream";
+                }
+            }
+
+            Response.Headers.Add("data-origin", dataOrigin);
+            return Ok(bookContent);
+        }
+    }
+}
---
+++ Services/RabbitMQListenerService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+public class RabbitMQListenerService : BackgroundService
+{
+    private readonly IConnectionFactory _connectionFactory;
+    private readonly IMemoryCache _memoryCache;
+    private readonly IDistributedCache _redisCache;
+
+    public RabbitMQListenerService(
+        IConnectionFactory connectionFactory,
+        IMemoryCache memoryCache,
+        IDistributedCache redisCache)
+    {
+        _connectionFactory = connectionFactory;
+        _memoryCache = memoryCache;
+        _redisCache = redisCache;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "bookQueue",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += async (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var bookId = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"book is HERE !");
+
+            var cacheKey = $"book_{bookId}";
+
+            _memoryCache.Remove(cacheKey);
+
+            await _redisCache.RemoveAsync(cacheKey);
+        };
+
+        channel.BasicConsume(queue: "bookQueue",
+            autoAck: true,
+            consumer: consumer);
+
+        await Task.CompletedTask;
+    }
+}
